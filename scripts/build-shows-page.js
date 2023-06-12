30774fab56c03e0f5883be4fefe714d39720a178
lucas-g-marins <lhintzmarins@gmail.com>
2023-06-12 15:37:04 -0700

    add in array/objects for showtimes

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,34 @@
+// list of upcoming shows
+
+let shows = [
+  {
+    Date: "Mon Sept 06 2021",
+    Venue: "Ronald Lane",
+    Location: "San Francisco, CA",
+  },
+  {
+    Date: "Tue Sept 21 2021",
+    Venue: "Pier 2 East",
+    Location: "San Francisco, CA",
+  },
+  {
+    Date: "Fri Oct 15 2021",
+    Venue: "View Lounge",
+    Location: "San Francisco, CA",
+  },
+  {
+    Date: "Sat Nov 6 2021",
+    Venue: "Hyatt Agency",
+    Location: "San Francisco, CA",
+  },
+  {
+    Date: "Fri Nov 26 2021",
+    Venue: "Moscow Center",
+    Location: "San Francisco, CA",
+  },
+  {
+    Date: "Wed Dec 15 2021",
+    Venue: "Press Club",
+    Location: "San Francisco, CA",
+  },
+];
