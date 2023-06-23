baf0d3b56ec02de4f726f8772bb31dc5ab09df49
lucas-g-marins <lhintzmarins@gmail.com>
2023-06-23 10:34:36 -0700

    add responsiveness to comment section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,7 +5,7 @@ let comments = [
     name: "Connor Walton",
     date: "02/17/2021",
     comment:
-      "This is art. This is inexplicable magicexpressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
@@ -17,7 +17,7 @@ let comments = [
     name: "Miles Acosta",
     date: "12/20/2020",
     comment:
-      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
+      "I can't stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
 
