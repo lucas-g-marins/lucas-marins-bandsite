6f1e2580255bf7cf628f1b72c420749e03aa84d5
lucas-g-marins <lhintzmarins@gmail.com>
2023-07-18 10:49:10 -0700

    post comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -12,52 +12,26 @@ const commentsURL = axios.get(
 commentsURL.then((comments) => {
   const commentsData = comments.data;
   console.log(commentsData);
-  const commentName = commentsData.name;
-  // const commentDate = setDate(comments.timestamp);
-  const commentInput = comments.comment;
-  comments.data.forEach((user) => {
-    console.log(user.name);
-  });
+
+  // post comments onto page
+
   displayComment(commentsData);
 });
 commentsURL.catch((error) => {
   console.log(error);
 });
 
-// array of objects for comments
-
-// let comments = [
-//   {
-//     name: "Connor Walton",
-//     date: "02/17/2021",
-//     comment:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//   },
-//   {
-//     name: "Emilie Beach",
-//     date: "01/09/2021",
-//     comment:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     date: "12/20/2020",
-//     comment:
-//       "I can't stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//   },
-// ];
-
 const commentForm = document.querySelector(".conversation__form");
 
 // function to set date
 
-function setDate(date) {
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  let fullDate = `${month}/${day}/${year}`;
-  return fullDate;
-}
+// function setDate(date) {
+//   let year = date.getFullYear();
+//   let month = date.getMonth() + 1;
+//   let day = date.getDate();
+//   let fullDate = `${month}/${day}/${year}`;
+//   return fullDate;
+// }
 
 // function for posting comments from array
 function displayComment(commentsData) {
@@ -99,7 +73,7 @@ function displayComment(commentsData) {
 
     // add date
     let date = document.createElement("p");
-    date.innerText = comment.date;
+    date.innerText = comment.timestamp;
     date.classList.add("conversation__date");
     nameDateDiv.appendChild(date);
 
@@ -111,7 +85,56 @@ function displayComment(commentsData) {
   });
 }
 
-displayComment();
+function postSingleComment(comment) {
+  // create div for comment values
+  let commentDiv = document.createElement("div");
+  commentDiv.classList.add("conversation__comment-container");
+
+  let conversationDiv = document.querySelector(
+    ".conversation__comment-section"
+  );
+  conversationDiv.prepend(commentDiv);
+
+  //   add div for avatar
+  let avatarDiv = document.createElement("div");
+  avatarDiv.classList.add("conversation__avatar-container");
+  commentDiv.appendChild(avatarDiv);
+
+  //   add avatar
+  let avatar = document.createElement("img");
+  avatar.classList.add("conversation__avatar");
+  avatarDiv.appendChild(avatar);
+
+  //   add div for name, date, and comment
+  let contentDiv = document.createElement("div");
+  contentDiv.classList.add("conversation__comment-content");
+  commentDiv.appendChild(contentDiv);
+
+  //   add div for name and date
+  let nameDateDiv = document.createElement("div");
+  nameDateDiv.classList.add("conversation__name-date-container");
+  contentDiv.appendChild(nameDateDiv);
+
+  //add name
+  let userName = document.createElement("p");
+  userName.innerText = comment.name;
+  userName.classList.add("conversation__name");
+  nameDateDiv.appendChild(userName);
+
+  // add date
+  let date = document.createElement("p");
+  date.innerText = comment.timestamp;
+  date.classList.add("conversation__date");
+  nameDateDiv.appendChild(date);
+
+  //   add div for comment
+  let commentValue = document.createElement("p");
+  commentValue.innerText = comment.comment;
+  commentValue.classList.add("conversation__comment");
+  contentDiv.appendChild(commentValue);
+}
+
+// displayComment();
 
 // PULL COMMENT
 
@@ -124,8 +147,8 @@ commentForm.addEventListener("submit", function (event) {
   const pullComment = event.target.comment.value;
   console.log(pullComment);
   // pull date
-  const pullDate = setDate(new Date());
-  console.log(pullDate);
+  // const pullDate = setDate(new Date());
+  // console.log(pullDate);
 
   // check for invalid inputs
   if (pullName.length === 0) {
@@ -138,34 +161,88 @@ commentForm.addEventListener("submit", function (event) {
     return false;
   } else {
     //   push values to comments array
-    const newComment = {
-      name: pullName,
-      date: pullDate,
-      comment: pullComment,
-    };
-    comments.unshift(newComment);
-    console.log(comments);
+    // const newComment = {
+    //   name: pullName,
+    //   date: pullDate,
+    //   comment: pullComment,
+    // };
+    // comments.unshift(newComment);
+    // console.log(comments);
+
+    // const newComment = axios.post(
+    //   `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`,
+    //   {
+    //     name: pullName,
+    //     comment: pullComment,
+    //   }
+    // );
+    // newComment.then((comments) => {
+    //   const commentsData = comments.data;
+    //   console.log(commentsData);
+
+    //   // post comments onto page
+
+    //   displayComment(commentsData);
+    // });
+    // newComment.catch((error) => {
+    //   console.log(error);
+    // });
 
     // Remove values from input fields
-    let clearName = document.querySelector(".conversation__input");
-    clearName.value = "";
+    // let clearName = document.querySelector(".conversation__input");
+    // clearName.value = "";
 
-    let clearComment = document.querySelector(".conversation__input--comment");
-    clearComment.value = "";
+    // let clearComment = document.querySelector(".conversation__input--comment");
+    // clearComment.value = "";
 
-    // Remove already posted comments
-    let commentsParent = document.querySelector(".conversation");
-    let postedComments = document.querySelector(
-      ".conversation__comment-section"
-    );
-    commentsParent.removeChild(postedComments);
+    // // Remove already posted comments
+    // let commentsParent = document.querySelector(".conversation");
+    // let postedComments = document.querySelector(
+    //   ".conversation__comment-section"
+    // );
+    // commentsParent.removeChild(postedComments);
 
-    // recreate comment section div
-    let newCommentSection = document.createElement("div");
-    newCommentSection.classList.add("conversation__comment-section");
-    commentsParent.appendChild(newCommentSection);
+    // // recreate comment section div
+    // let newCommentSection = document.createElement("div");
+    // newCommentSection.classList.add("conversation__comment-section");
+    // commentsParent.appendChild(newCommentSection);
 
     // Post all comments from array
-    displayComment();
+    // commentsURL.then((comments) => {
+    //   const commentsData = comments.data;
+    //   console.log(commentsData);
+
+    //   // post comments onto page
+
+    //   displayComment(commentsData);
+    // });
+    // commentsURL.catch((error) => {
+    //   console.log(error);
+    // });
+
+    const newComment = axios.post(
+      `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`,
+      {
+        name: pullName,
+        comment: pullComment,
+      }
+    );
+    newComment.then((result) => {
+      console.log(result);
+      // commentsURL.then((comments) => {
+      //   const commentsData = comments.data;
+      //   console.log(commentsData);
+
+      //   // post comments onto page
+
+      //   displayComment(commentsData);
+      // });
+      const newCommentData = result.data;
+      console.log(newCommentData);
+      postSingleComment(newCommentData);
+    });
+    newComment.catch((error) => {
+      console.log(error);
+    });
   }
 });
